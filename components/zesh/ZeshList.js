fb57e622a72a92058c18a100d49ff6d67287a5b9
Alessandro Carinato <alessandrocarinato@Alessandros-MacBook-Pro.local>
2021-09-21 17:15:37 +0200

    link to zesh detail

--- components/zesh/ZeshList.js
+++ components/zesh/ZeshList.js
@@ -1,9 +1,11 @@
 import { Container, Grid, Typography } from '@material-ui/core';
 import React from 'react';
+import { useRouter } from 'next/router';
 import useStyles from '../../utils/styles';
 import ZeshItem from './ZeshItem';
 
 export default function ZeshList(props) {
+  const router = useRouter();
   const classes = useStyles();
   return (
     <Container>
@@ -13,7 +15,14 @@ export default function ZeshList(props) {
       <Grid container>
         {props.zeshes.map((zesh) => (
           <Grid key={zesh.id} item xs={12} sm={4}>
-            <ZeshItem key={zesh.id} id={zesh.id} title={zesh.title} />
+            <div
+              className={classes.zeshLink}
+              onClick={() => {
+                router.push('/' + zesh.id);
+              }}
+            >
+              <ZeshItem key={zesh.id} id={zesh.id} title={zesh.title} />
+            </div>
           </Grid>
         ))}
       </Grid>
---
+++ pages/[zeshId]/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function ZeshDetails() {
+  return <div></div>;
+}
