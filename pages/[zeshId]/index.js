8fcf7dab8ba655b0b20b6367adfc46e91e10571b
Alessandro Carinato <alessandrocarinato@Alessandros-MacBook-Pro.local>
2021-09-22 09:29:58 +0200

    added get static path and props

--- pages/[zeshId]/index.js
+++ pages/[zeshId]/index.js
@@ -1,5 +1,38 @@
-import React from 'react';
+import ZeshItemDetails from '../../components/zesh/ZeshItemDetails';
 
-export default function ZeshDetails() {
-  return <div></div>;
+function ZeshDetails() {
+  return <ZeshItemDetails title="Tutto Tasse" />;
 }
+
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          zeshId: 'z1',
+        },
+      },
+      {
+        params: {
+          zeshId: 'z2',
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const zeshId = context.params.zeshId;
+
+  return {
+    props: {
+      zeshData: {
+        id: zeshId,
+        title: 'Tette Tosse',
+      },
+    },
+  };
+}
+
+export default ZeshDetails;
---
+++ pages/api/new-zesh.js
@@ -0,0 +1,8 @@
+function handler(req, res) {
+  if (req.method === 'POST') {
+    const data = req.body;
+    const { title } = data;
+  }
+}
+
+export default handler;
